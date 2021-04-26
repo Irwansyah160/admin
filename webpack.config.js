b3e5a832d4e99750387593040eb78f18a1762377
Ibadd <79823545+Ibadd@users.noreply.github.com>
2021-04-26 19:39:07 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -118,7 +118,7 @@ module.exports = {
   devServer: {
     contentBase: Path.join(__dirname, "static"),
     compress: true,
-    port: 8080,
+    port: listen(process.env.PORT || 8080),
     open: true
   }
 };
